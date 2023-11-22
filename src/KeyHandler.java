ea15ef8bb19247761fabe74c654e7cbd4b2f9dc9
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-22 14:54:43 -0500

    Black screen generated

---
+++ src/KeyHandler.java
@@ -0,0 +1,48 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode(); // returns the integer keycode assocaited with the key that is pressed
+        if (code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (code == KeyEvent.VK_A) {
+            downPressed = true;
+        }
+        if (code == KeyEvent.VK_S) {
+            leftPressed = true;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) { // jump
+            upPressed = false;
+        }
+        if (code == KeyEvent.VK_A) {
+            downPressed = false;
+        }
+        if (code == KeyEvent.VK_S) { // crouch
+            leftPressed = false;
+        }
+        if (code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
+    
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicComboBoxUI.KeyHandler;
+
+public class GamePanel extends JPanel {
+    // Screen settings
+
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 px
+    final int screenHeight = tileSize * maxScreenRow; // 576 px
+
+    KeyHandler keyH;
+
+    //BasicComboBoxUI box = new BasicComboBoxUI();
+    //KeyHandler keyH = box.new KeyHandler();
+    Thread gameThread; // keeps program running untill manually stopped
+
+    //set players default postion
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+}
