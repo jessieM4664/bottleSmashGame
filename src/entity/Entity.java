a8a74e3caf41658708815c3194d610d4b5ceacdc
Jessica Morrison <jessicamorrison8@rocketmail.com>
2023-11-24 20:40:02 -0500

    moving feet on avatar added

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -7,5 +7,8 @@ public class Entity {
     public int speed;
     public BufferedImage left1, left2, right1, right2;
     public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
 
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -30,8 +30,10 @@ public class Player extends Entity {
 
     public void getPlayerImage() {
         try {
-            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/avatar_left1.PNG"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/avatar_right1.PNG"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left1.PNG"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_left2.PNG"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right1.PNG"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/player/boy_right2.PNG"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -49,6 +51,15 @@ public class Player extends Entity {
             direction = "right";
             x += speed;
         }
+        spriteCounter++;
+        if (spriteCounter > 10) {
+            if (spriteNum ==1) {
+                spriteNum = 2;
+            } else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
 
     public void draw(Graphics2D g2) {
@@ -58,10 +69,20 @@ public class Player extends Entity {
 
         switch(direction) {
         case "left":
-            image = left1;
+            if (spriteNum == 1) {
+                image = left1;
+            }
+            if (spriteNum == 2) {
+                image = left2;
+            }
             break;
         case "right":
-            image = right1;
+            if (spriteNum == 1) {
+                image = right1;
+            }
+            if (spriteNum == 2) {
+                image = right2;
+            }
             break;
         }
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
